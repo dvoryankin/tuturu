81fa3f044ca6303260f50bf97dd41963b9ec2884
pchellovod <pchellovod@gmail.com>
2016-10-31 15:59:44 +0500

    add callback set_name

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,6 +1,17 @@
 class Route < ActiveRecord::Base
+
+
   # has_and_belongs_to_many :railway_stations
   has_many :railway_stations_routes
   has_many :railway_stations, through: :railway_stations_routes
+
   validates :title, presence: true
+
+  before_validation :set_name
+
+  private
+
+  def set_name
+    self.name ||= "#{railway_stations.first.name} - #{railway_stations.last.name}"
+  end
 end
