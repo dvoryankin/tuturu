a0b548be828653a9ba88db566ed01e9b60bad292
pchellovod <pchellovod@gmail.com>
2016-11-03 03:10:52 +0500

    change callback on before

--- app/models/route.rb
+++ app/models/route.rb
@@ -12,6 +12,6 @@ class Route < ActiveRecord::Base
   private
 
   def set_name
-    self.name ||= "#{railway_stations.first.name} - #{railway_stations.last.name}"
+    self.name = "#{railway_stations.first.title} - #{railway_stations.last.title}"
   end
 end
